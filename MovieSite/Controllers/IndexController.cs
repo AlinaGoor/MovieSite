7e5903c58ee55fd081f680c296168def39eae147
Mate Balint <matebalint98@gmail.com>
2019-06-13 09:21:12 +0200

    Removed unused namespaces

--- MovieSite/Controllers/IndexController.cs
+++ MovieSite/Controllers/IndexController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MovieSite.Models;
 
@@ -9,27 +7,27 @@ namespace MovieSite.Controllers
 {
     public class IndexController : Controller
     {
-        private Movie _actualMovie = new Models.Movie()
+        private Movie _actualMovie = new Movie()
         {
-            ID = 1,
+            Id = 1,
             Title = "The Godfather",
             ReleaseDate = new DateTime(1972, 3, 24)
         };
-        private Movie _actualMovie2 = new Models.Movie()
+        private Movie _actualMovie2 = new Movie()
         {
-            ID = 2,
+            Id = 2,
             Title = "The Godfathere",
             ReleaseDate = new DateTime(1972, 4, 24)
         };
 
-        private IEnumerable<Movie> _allOfMovies = new Movie[]{new Models.Movie()
+        private IEnumerable<Movie> _allOfMovies = new Movie[]{new Movie()
             {
-                ID = 2,
+                Id = 2,
                 Title = "One",
                 ReleaseDate = new DateTime(1972, 4, 24)
-            }, new Models.Movie()
+            }, new Movie()
             {
-                ID = 1,
+                Id = 1,
                 Title = "TWo",
                 ReleaseDate = new DateTime(1972, 3, 24)
             }
