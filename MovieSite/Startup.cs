e0d9e38e52c2aa50f710d70041684dd3f4f58e60
AlinaGoor <gooralina@gmail.com>
2019-06-13 10:48:06 +0200

    Base code for Postgres, still need migration

--- MovieSite/Startup.cs
+++ MovieSite/Startup.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MovieSite.Models;
 
 namespace MovieSite
 {
@@ -13,10 +17,20 @@ namespace MovieSite
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            var connString = "Host=localhost;Port=5432;Username=postgres;Password=1234;Database=prays";
+            services.AddDbContext<MovieDBContext>(options => options.UseNpgsql(connString));
             services.AddMvc();
         }
+        
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
